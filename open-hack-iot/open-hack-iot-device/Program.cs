740e452b69ccd5c0ecbc10a8314565c23f5d775e
shajusukumaran <shaju.sukumaran@hotmail.com>
2018-11-06 13:47:54 -0500

    updated project

---
+++ open-hack-iot/open-hack-iot-device/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace open_hack_iot_device
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
